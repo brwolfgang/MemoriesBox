75740183b6985df871420fa4274199cccac93512
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-03-16 21:36:27 -0400

    Initial source push

---
+++ app/src/androidTest/java/br/nom/marcos/wolfgang/android/memoriesbox/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.nom.marcos.wolfgang.android.memoriesbox;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+  public ApplicationTest() {
+    super(Application.class);
+  }
+}
