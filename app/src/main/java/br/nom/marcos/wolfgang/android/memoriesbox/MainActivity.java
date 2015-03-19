5827c597d6fa31057169a395ec03b6e40387d744
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-03-19 15:54:18 -0400

    Adds initial support for a main screen with a list of memories

--- app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
@@ -1,5 +1,7 @@
 package br.nom.marcos.wolfgang.android.memoriesbox;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
 
 import java.sql.SQLException;
 
@@ -56,11 +58,27 @@ public class MainActivity extends ActionBarActivity {
   @Override
   protected void onPostResume() {
     super.onPostResume();
-    this.initDatabase();
   }
 
   private void initResources(){
     this.initDatabase();
+    this.memoryListView = (ListView) findViewById(R.id.main_memory_list_view);
+
+    Cursor memoryCursor = mds.getAllMemories();
+
+    CursorAdapter cursorAdapter = new MemoriesListAdapter(this,
+          memoryCursor, MemoriesListAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+    memoryListView.setAdapter(cursorAdapter);
+
+    Button button = (Button) findViewById(R.id.main_add_memory);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(),MemoryDetailsViewer.class);
+        startActivity(intent);
+      }
+    });
   }
 
   private void initDatabase(){
@@ -75,5 +93,7 @@ public class MainActivity extends ActionBarActivity {
   }
 
   private static final String TAG = "MainActivity";
-  MemoriesDataSource mds;
+  private MemoriesDataSource mds;
+  private ListView memoryListView;
+
 }
