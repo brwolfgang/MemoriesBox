45b2706faf90d04e3bdd3fa1e9d74bae2b2b1610
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-03-20 21:59:26 -0400

    Removes unused overrides and imports on MainActivity

--- app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
@@ -2,11 +2,9 @@ package br.nom.marcos.wolfgang.android.memoriesbox;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CursorAdapter;
@@ -23,29 +21,6 @@ public class MainActivity extends ActionBarActivity {
     setContentView(R.layout.activity_main);
 
     initResources();
-  }
-
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 
   @Override
