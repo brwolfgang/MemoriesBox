da1719c6a73c3e80a3651fefd5a4c7c68df75a7e
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-03-24 12:50:47 -0400

    Adds TimePickerFragment

---
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/TimePickerFragment.java
@@ -0,0 +1,32 @@
+package br.nom.marcos.wolfgang.android.memoriesbox;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+/**
+ * Created by Wolfgang Marcos on 24/03/2015.
+ */
+public class TimePickerFragment extends DialogFragment{
+
+  TimePickerDialog.OnTimeSetListener listener;
+
+  @Override
+  public void onAttach(Activity activity) {
+    super.onAttach(activity);
+    this.listener = (TimePickerDialog.OnTimeSetListener) activity;
+  }
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Calendar calendar = Calendar.getInstance();
+    int hour = calendar.get(Calendar.HOUR);
+    int minute = calendar.get(Calendar.MINUTE);
+
+    return new TimePickerDialog(getActivity(), listener, hour, minute, true);
+  }
+}
