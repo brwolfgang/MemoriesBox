f71ca53c2a454009fcde8308ea535ec1de2f23f9
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-03-25 11:10:01 -0400

    Adds ActionBar to MainActivity

--- app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -51,6 +52,12 @@ public class MainActivity extends ActionBarActivity implements MemoriesRetrieveT
   public void onPostExecute(Cursor cursor) {
     memoriesAdapter.changeCursor(cursor);
     memoriesAdapter.notifyDataSetChanged();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return super.onCreateOptionsMenu(menu);
   }
 
   private void initResources(){
