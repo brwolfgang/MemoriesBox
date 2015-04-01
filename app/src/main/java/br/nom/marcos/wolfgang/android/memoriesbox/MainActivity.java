a4caadb23e756cd7d3f4699c658c0fdc28609e40
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-04-01 16:59:05 -0400

    Adds initial MainActivity Fragments handling
    - MemoryList is created and allocated when the Activity starts
    - Swapping Fragments when a create a new memory command is called
    - MainActivity receives date and time picks and passes them to MemoryDetailsViewerFragment

--- app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/MainActivity.java
@@ -1,14 +1,17 @@
 package br.nom.marcos.wolfgang.android.memoriesbox;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 
-public class MainActivity extends ActionBarActivity implements
-    MemoryListFragment.MemoryListFragmentListener, MemoryDetailsViewerFragment.MemoryDetailsViewerFragmentListener {
+public class MainActivity extends ActionBarActivity implements MemoryListFragment.MemoryListFragmentListener, MemoryDetailsViewerFragment.MemoryDetailsViewerFragmentListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
   private static final String TAG = "MainActivity";
   private FragmentManager fragmentManager;
@@ -35,7 +38,21 @@ public class MainActivity extends ActionBarActivity implements
 
   @Override
   public void createNewMemory() {
+    fragmentTransaction = fragmentManager.beginTransaction();
+    memoryDetailsViewer = new MemoryDetailsViewerFragment();
+    fragmentTransaction.replace(R.id.main_fragment_container, memoryDetailsViewer);
+    fragmentTransaction.addToBackStack(null);
+    fragmentTransaction.commit();
+  }
 
+  @Override
+  public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+    ((DatePickerDialog.OnDateSetListener) memoryDetailsViewer).onDateSet(view, year, monthOfYear, dayOfMonth);
+  }
+
+  @Override
+  public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+    ((TimePickerDialog.OnTimeSetListener) memoryDetailsViewer).onTimeSet(view, hourOfDay, minute);
   }
 
   private void initResources() {
