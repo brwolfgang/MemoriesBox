3cfdea4e7c543882923a36e26229876dc96217d9
Wolfgang Marcos <wolfgang.tw@gmail.com>
2015-04-13 22:19:12 -0400

    Cleans database creation process.

--- app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/DatabaseUtil.java
+++ app/src/main/java/br/nom/marcos/wolfgang/android/memoriesbox/DatabaseUtil.java
@@ -17,23 +17,25 @@ public class DatabaseUtil extends SQLiteOpenHelper{
   public static final String MEMORY_COLUMN_CONTENT = "content";
   public static final String MEMORY_COLUMN_DATE = "date";
   public static final String MEMORY_COLUMN_TIME = "time";
+  private static final String CREATE_TABLE_MEMORY = "CREATE TABLE "
+      + MEMORY_TABLE_NAME + "("
+      + MEMORY_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+      + MEMORY_COLUMN_TITLE + " TEXT NOT NULL, "
+      + MEMORY_COLUMN_CONTENT + " TEXT NOT NULL, "
+      + MEMORY_COLUMN_DATE + " TEXT NOT NULL, "
+      + MEMORY_COLUMN_TIME + " TEXT NOT NULL);";
   public static final String IMAGE_TABLE_NAME =  "image";
   public static final String IMAGE_COLUMN_ID = "_id";
   public static final String IMAGE_COLUMN_MEMORY_ID = "memory_id";
   public static final String IMAGE_COLUMN_NAME = "image_path";
-  private static final String DATABASE_CREATE = "create table "
-      + MEMORY_TABLE_NAME + "("
-      + MEMORY_COLUMN_ID + " integer primary key autoincrement, "
-      + MEMORY_COLUMN_TITLE + " text not null, "
-      + MEMORY_COLUMN_CONTENT + " text not null, "
-      + MEMORY_COLUMN_DATE + " text not null, "
-      + MEMORY_COLUMN_TIME + " text not null); "
-      + "create table "
+  private static final String CREATE_TABLE_IMAGES = "CREATE TABLE "
       + IMAGE_TABLE_NAME + "("
-      + IMAGE_COLUMN_ID + " integer primary key autoincrement, "
-      + IMAGE_COLUMN_MEMORY_ID + " integer not null, "
-      + IMAGE_COLUMN_NAME+ " text not null);";
+      + IMAGE_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+      + IMAGE_COLUMN_MEMORY_ID + " INTEGER NOT NULL, "
+      + IMAGE_COLUMN_NAME+ " TEXT NOT NULL);";
   private static final String TAG = "DatabaseUtil";
+
+
 
   public DatabaseUtil(Context context) {
     super(context, MEMORY_TABLE_NAME, null, 1);
@@ -42,11 +44,12 @@ public class DatabaseUtil extends SQLiteOpenHelper{
   @Override
   public void onCreate(SQLiteDatabase db) {
     try {
-      Log.i(TAG, "Going to execute: " + DATABASE_CREATE);
-      db.execSQL(DATABASE_CREATE);
-      Log.i(TAG, "Database " + MEMORY_TABLE_NAME + " created successfully");
+      Log.i(TAG, "Creating database");
+      db.execSQL(CREATE_TABLE_MEMORY);
+      db.execSQL(CREATE_TABLE_IMAGES);
+      Log.i(TAG, "Database created successfully");
     } catch (SQLException e) {
-      Log.i(TAG, "Database " + MEMORY_TABLE_NAME + " creation failed");
+      Log.i(TAG, "Database creation failed");
       e.printStackTrace();
       System.err.println(e.getCause() == null ? "No cause was given" : e.getCause());
     }
